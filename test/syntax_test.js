3a0b99a81c8601edcf4ff52cd0368ae04075ec02
Carleen Petrosian <cpetrosi@lion.lmu.edu>
2017-03-01 20:54:40 -0800

    more tests added

--- test/syntax_test.js
+++ test/syntax_test.js
@@ -8,12 +8,54 @@ function parse(string) {
 }
 
 describe('QUEEN', () => {
-    it('👑 helloWorld 🚀 🖨 "Hello World!" should be a valid function', () => {
-        const match = parse('👑 helloWorld 🚀 🖨 "Hello World!"');
+    it('👑 string (helloWorld : string) 🚀 🖨 "Hello World!" should be a valid function', () => {
+        const match = parse('👑 string (helloWorld : string) 🚀 🖨 "Hello World!"');
         assert.ok(match.succeeded());
     });
-    it('👑 helloWorld 🖨 "Hello World!" should not be a valid function', () => {
-        const match = parse('👑 helloWorld 🖨 "Hello World!"');
+
+    it('👑 string (helloWorld : string) 🖨 "Hello World!" should not be a valid function', () => {
+        const match = parse('👑 string (helloWorld : string) 🖨 "Hello World!"');
         assert.ok(match.failed());
     });
+
+    it('👑 int (f145 : int) 🚀 match f145 with 🍭 f145 -> "Success!" should be a valid function', () => {
+        const match = parse('👑 int (f145 : int) 🚀 match f145 with 🍭 f145 -> "Success!"');
+        assert.ok(match.succeeded());
+    });
+
+    it('👑 bool (odd : int) 🚀 match odd with 🍭 3 -> true 🍭 4 -> false should be a valid function', () => {
+        const match = parse('👑 bool (odd : int) 🚀 match odd with 🍭 3 -> true 🍭 4 -> false');
+        assert.ok(match.succeeded());
+    });
+
+    it('👑 int (x : int) 🚀 let x 🚀 4 + 5 in 5 should be a valid function', () => {
+        const match = parse('👑 int (x : int) 🚀 let x 🚀 4 + 5 in 5');
+        assert.ok(match.succeeded());
+    });
+
+    it('👑 float (x : float) 🚀 let floatList 🚀 x @ [1.0, 2.0] should be a valid function', () => {
+        const match = parse('👑 float (x : float) 🚀 let floatList 🚀 x @ [1.0, 2.0]');
+        assert.ok(match.succeeded());
+    });
+
+    it('👑 int (f145 : int) 🚀 f145 with 🍭 f145 -> "Success!" should not be a valid function', () => {
+        const match = parse('👑 int (f145 : int) 🚀 f145 with 🍭 f145 -> "Success!"');
+        assert.ok(match.failed());
+    });
+
+    it('bool (odd : int) 🚀 match odd with 🍭 3 -> true 🍭 4 -> false should not be a valid function', () => {
+        const match = parse('bool (odd : int) 🚀 match odd with 🍭 3 -> true 🍭 4 -> false');
+        assert.ok(match.failed());
+    });
+
+    it('👑 int (x) 🚀 let x 🚀 4 + 5 in 5 should not be a valid function', () => {
+        const match = parse('👑 int (x) 🚀 let x 🚀 4 + 5 in 5');
+        assert.ok(match.failed());
+    });
+
+    it('👑 float (x : float) let floatList 🚀 x @ [1.0, 2.0] should not be a valid function', () => {
+        const match = parse('👑 float (x : float) let floatList 🚀 x @ [1.0, 2.0]');
+        assert.ok(match.failed());
+    });
+
 });
