0a96afb67483cd83d2459a715c19d58462242e8c
maryalverson <malverso@lion.lmu.edu>
2017-03-02 14:08:36 -0800

    About to merge jordan and my parser.js eek!

--- parser.js
+++ parser.js
@@ -4,94 +4,104 @@ const BinExpAdd = require('./entities/binexp_add');
 const BinExpExp = require('./entities/binexp_exp');
 const Body = require('./entities/body');
 const Conditional = require('./entities/conditional');
-const Cons = require('./entities/cons');
+const ConsNil = require('./entities/cons_nil');
+const ConsShort = require('./entities/cons_short');
+const ConsLong = require('./entities/cons_long');
 const ExpAppend = require('./entities/exp_append');
 const ExpBinExp = require('./entities/exp_binexp');
 const ExpBool = require('./entities/exp_bool');
 const ExpConditional = require('./entities/exp_conditional');
 const ExpId = require('./entities/exp_id');
-const ExpLet = require('./entities/expLet');
-const ExpMatch = require('./entities/expMatch');
-const ExpNumLit = require('./entities/numLit');
-const ExpParens = require('./entities/expParens');
-const ExpPrint = require('./entities/expPrint');
-const ExpString = require('./entities/expString');
-const Exp1Exp = require('./entities/exp1Exp');
-const Exp1Mult = require('./entities/exp1Mult');
-const Exp2 = require('./entities/Exp2');
-const Exp3 = require('./entities/Exp3');
-const Funcall = require('./entities/funCall');
-const FunDecl = require('./entities/funDecl');
-const Let = require('./entities/Let');
-const List = require('./entities/List');
-const ListElements = require('./entities/listElements');
-const Match = require('./entities/Match');
-const PatternCons = require('./entities/patternCons');
-const PatternPattern = require('./entities/patternPattern');
-const PatternWild = require('./entities/patternWild');
-const Program = require('./entities/Program');
-const TypeBool = require('./entities/typeBool');
-const TypeFloat = require('./entities/typeFloat');
-const TypeInt = require('./entities/typeInt');
-const TypeList = require('./entities/typeList');
-const TypeString = require('./entities/typeString');
-const TypeTuple = require('./entities/typeTuple');
+const ExpLet = require('./entities/exp_let');
+const ExpMatch = require('./entities/exp_match');
+const ExpNumLit = require('./entities/exp_numlit');
+const ExpParens = require('./entities/exp_parens');
+const ExpPrint = require('./entities/exp_print');
+const ExpString = require('./entities/exp_string');
+const Exp1Exp = require('./entities/exp1_exp');
+const Exp1Mult = require('./entities/exp1_mult');
+const Exp2 = require('./entities/exp2');
+const Exp3 = require('./entities/exp3');
+const Funcall = require('./entities/funcall');
+const FunDecl = require('./entities/fundecl');
+const LetExp = require('./entities/let_exp');
+const LetLet = require('./entities/let_let');
+const List = require('./entities/list');
+const ListElements = require('./entities/listelements');
+const Match = require('./entities/match');
+const MatchExp = require('./entities/matchexp');
+const PatternCons = require('./entities/pattern_cons');
+const PatternPattern = require('./entities/pattern_pattern');
+const PatternWild = require('./entities/pattern_wild');
+const Program = require('./entities/program');
+const TypeBool = require('./entities/type_bool');
+const TypeFloat = require('./entities/type_float');
+const TypeInt = require('./entities/type_int');
+const TypeList = require('./entities/type_list');
+const TypeString = require('./entities/type_string');
+const TypeTuple = require('./entities/type_tuple');
+const ohm = require('ohm-js');
+const fs = require('fs');
 
-const ohm = require ('ohm-js');
-const fs = require ('entities/fs');
-const grammar = ohm.grammar(fs.readFileSync ('./syntax.ohm'));
+const grammar = ohm.grammar(fs.readFileSync('./syntax.ohm'));
 
-const semantics = aelGrammar.createSemantics().addOperation('ast', {
+const semantics = grammar.createSemantics().addOperation('ast', {
   Program(FunDecls) { return new Program(FunDecls.ast()); },
   FunDecl(crown, type, left, bindings, right, rocket, body) {
       return new FunDecl(bindings.ast(), body.ast());
   },
-  Binding(id, colon, type) { return new Binding(id.ast(), type.ast()); },
+  Binding(id, colon, type) { return new Binding(id.sourceString, type.ast()); },
   Body(arg) { return new Body(arg.ast()); },
   Exp_append(body) { return new ExpAppend(body.ast()); },
   Exp_binexp(body) { return new ExpBinExp(body.ast()); },
   Exp_bool(body) { return new ExpBool(body.ast()); },
   Exp_conditional(body) { return new ExpConditional(body.ast()); },
   Exp_id(body) { return new ExpId(body.ast()); },
-  Exp_let(body) { return new expId(body.ast()); },
-  Exp_match(body) { return new expMatch(body.ast()); },
-  Exp_numlit(body) { return new expNumLit(body.ast()); },
-  Exp_parens(right, body, left) { return new expParens(body.ast()); },
-  Exp_print(printer, string) { return new expPrint(string.ast()); },
-  Exp_string(body) { return new expString(body.ast()); },
-  Pattern_cons(cons) { return new patternCons(cons.sourceString); },
-  Pattern_wild(blackHole) { return new patternWild(blackHole.sourceString); },
+  Exp_let(body) { return new ExpLet(body.ast()); },
+  Exp_match(body) { return new ExpMatch(body.ast()); },
+  Exp_numlit(body) { return new ExpNumLit(body.ast()); },
+  Exp_parens(right, body, left) { return new ExpParens(body.ast()); },
+  Exp_print(printer, string) { return new ExpPrint(string.sourceString); },
+  Exp_string(body) { return new ExpString(body.ast()); },
+  Pattern_cons(cons) { return new PatternCons(cons.sourceString); },
+  Pattern_wild(blackHole) { return new PatternWild(blackHole.sourceString); },
   Pattern_pattern(left, first, colon, rest, right) {
-      return new patternPattern(first.ast(), rest.ast());
+      return new PatternPattern(first.ast(), rest.ast());
   },
-  Cons_long(id, lightning, rest) { return new consLong(id.ast(), rest.ast()); },
-  Cons_short(id, lightning, empty) { return new consShort(id.ast()); },
-  Cons_nil(nil) { return new consNil(nil.ast()); },
-  Funcall(id, left, params, commas, right) { return new funCall(id.sourceString, params.sourceString); },
+  Cons_long(id, lightning, rest) { return new ConsLong(id.sourceString, rest.ast()); },
+  Cons_short(id, lightning, empty) { return new ConsShort(id.sourceString); },
+  Cons_nil(nil) { return new ConsNil(nil.ast()); },
+  Funcall(id, left, params, commas, right) { return new Funcall(id.sourceString, params.sourceString); },
   List(left, elements, right) { return new List(elements.ast()); },
-  ListElements(first, commas, rest) { return new listElements(first.sourceString, rest.sourceString); },
+  ListElements(first, commas, rest) { return new ListElements(first.sourceString, rest.sourceString); },
   Match(match, id, _, exps) { return new Match(id.sourceString, exps.ast()); },
-  MatchExp(pop, pattern, arrow, exp) { return new matchExp(pattern.ast(), exp.ast()); },
-  Let_let(let1, id, rocket, exp, _, let2) { return new letLet(id.sourceString, exp.ast(), let2.ast()); },
-  Let_exp(exp) { return new letExp(exp.ast()); },
+  MatchExp(pop, pattern, arrow, exp) { return new MatchExp(pattern.ast(), exp.ast()); },
+  Let_let(let1, id, rocket, exp, _, let2) { return new LetLet(id.sourceString, exp.ast(), let2.ast()); },
+  Let_exp(exp) { return new LetExp(exp.ast()); },
   Append(listOne, at, listTwo) { return new Append(listOne.ast(), listTwo.ast()); },
-  BinExp_add(exp1, addop, binexp) { return new BinExpAdd(exp1.ast(), addop.sourceString, binexp.ast()); },
-  BinExp_exp(exp1) { return new BinExpExp(exp1.ast()); },
+  Binexp_add(exp1, addop, binexp) { return new BinExpAdd(exp1.ast(), addop.sourceString, binexp.ast()); },
+  Binexp_exp(exp1) { return new BinExpExp(exp1.ast()); },
   Exp1_mult(exp1, op, exp2) {
-      return new exp1Mult(exp1.ast(). op.sourceString, exp2.ast());
+      return new Exp1Mult(exp1.ast(), op.sourceString, exp2.ast());
   },
-  Exp1_exp(exp) { return new exp1Exp(exp.ast()); },
+  Exp1_exp(exp) { return new Exp1Exp(exp.ast()); },
   Exp2(negative, exp) { return new Exp2(negative.sourceString, exp.ast()); },
   Exp3(num, factorial) { return new Exp3(num.sourceString, factorial.sourceString); },
-  Conditional(i, Exp1, ifLogical, Exp2s, then1, Exp3, elseif, Exp4, elseIfLogical, Exp5s,
+  Conditional(i, Exp1, ifLogical, Exp2s, then1, ExpThree, elseif, Exp4, elseIfLogical, Exp5s,
       then2, Exp6, e, Exp7) {
-      return new Conditional(Exp1.ast(), ifLogical.ast(), Exp2s.ast(), Exp3.ast(), Exp4.ast(),
+      return new Conditional(Exp1.ast(), ifLogical.ast(), Exp2s.ast(), ExpThree.ast(), Exp4.ast(),
         elseIfLogical.ast(), Exp5s.ast(), Exp6.ast(), Exp7.ast());
   },
-  Type_list(type, list) { return new typeList(type.ast()); },
-  Type_tuple(type1, start, type2) { return new typeTuple(type1.ast(), type2.ast()); },
-  Type_bool(bool) { return new typeBool(bool.sourceString); },
-  Type_int(int) { return new typeInt(int.sourceString); },
-  Type_float(float){ return new typeFloat(float.sourceString); },
-  Type_string(string) { return new typeString(string.sourceString); }
+  Type_list(type, list) { return new TypeList(type.ast()); },
+  Type_tuple(type1, start, type2) { return new TypeTuple(type1.ast(), type2.ast()); },
+  Type_bool(bool) { return new TypeBool(bool.sourceString); },
+  Type_int(int) { return new TypeInt(int.sourceString); },
+  Type_float(float) { return new TypeFloat(float.sourceString); },
+  Type_string(string) { return new TypeString(string.sourceString); },
 });
+
+function parse(text) {
+    const match = grammar.match(text);
+    return semantics(match).ast();
+}
+module.exports = parse;
