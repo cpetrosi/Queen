2bb0a030a507ecc6d10965e91b6faf38940c1870
Mary Alverson <maryalverson@users.noreply.github.com>
2017-03-02 22:31:33 -0800

    Runs parser from cmd

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Hello Queen');
+const parse = require('./parser');
+const fs = require('fs');
+
+const path = fs.readFileSync(`${process.argv[2]}`);
+const ast = parse(path).toString();
+console.log(ast);
