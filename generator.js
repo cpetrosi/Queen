b7d9864c6e651daa6444f90d64a92afe78c6c38d
maryalverson <malverso@lion.lmu.edu>
2017-04-24 17:13:19 -0700

    Imported files for generator.

---
+++ generator.js
@@ -0,0 +1,48 @@
+const Append = require('./entities/append');
+const Binding = require('./entities/binding');
+const BinExpAdd = require('./entities/binexp_add');
+const BinExpRel = require('./entities/binexp_rel');
+const BinExpExp = require('./entities/binexp_exp');
+const Body = require('./entities/body');
+const Conditional = require('./entities/conditional');
+const ConsNil = require('./entities/cons_nil');
+const ConsShort = require('./entities/cons_short');
+const ConsLong = require('./entities/cons_long');
+const ExpAppend = require('./entities/exp_append');
+const ExpBinExp = require('./entities/exp_binexp');
+const ExpBool = require('./entities/exp_bool');
+const ExpConditional = require('./entities/exp_conditional');
+const ExpId = require('./entities/exp_id');
+const ExpLet = require('./entities/exp_let');
+const ExpMatch = require('./entities/exp_match');
+const ExpNumLit = require('./entities/exp_numlit');
+const ExpParens = require('./entities/exp_parens');
+const ExpPrint = require('./entities/exp_print');
+const ExpString = require('./entities/exp_string');
+const Exp1Exp = require('./entities/exp1_exp');
+const Exp1Mult = require('./entities/exp1_mult');
+const Exp2 = require('./entities/exp2');
+const Exp3 = require('./entities/exp3');
+const Funcall = require('./entities/funcall');
+const FunDecl = require('./entities/fundecl');
+const LetExp = require('./entities/let_exp');
+const LetLet = require('./entities/let_let');
+const List = require('./entities/list');
+const Match = require('./entities/match');
+const MatchExp = require('./entities/matchexp');
+const PatternCons = require('./entities/pattern_cons');
+const PatternPattern = require('./entities/pattern_pattern');
+const PatternWild = require('./entities/pattern_wild');
+const Program = require('./entities/program');
+const TypeBool = require('./entities/type_bool');
+const TypeFloat = require('./entities/type_float');
+const TypeInt = require('./entities/type_int');
+const TypeList = require('./entities/type_list');
+const TypeString = require('./entities/type_string');
+const TypeTuple = require('./entities/type_tuple');
+
+Object.assign(Program.prototype, {
+  gen() {
+    this.FunDecls.forEach(funDecl => funDecl.gen());
+  },
+});
